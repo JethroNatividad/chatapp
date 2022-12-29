b3a89885397d90631ff5362c38f3a4e1f82e662c
JethroSama <kel73896@gmail.com>
2022-12-30 00:58:47 +0800

    Create message model

---
+++ models/Message.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const MessageSchema = new mongoose.Schema({
+  chat: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Chat',
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  text: {
+    type: String,
+  },
+  attachments: [{
+    type: String,
+  }],
+
+},
+  {
+    timestamps: true,
+  })
+
+export default mongoose.models.Message || mongoose.model('Message', MessageSchema)
