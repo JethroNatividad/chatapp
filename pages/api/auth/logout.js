8470aa9169f26c621d0355dffdbac0ffc4ee0600
JethroSama <kel73896@gmail.com>
2022-12-31 14:46:42 +0800

    implement logout

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,50 +1,42 @@
-import dbConnect from '../../../lib/dbConnect'
-import User from '../../../models/User'
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import fetch from 'isomorphic-unfetch'
-import { deleteCookie, hasCookie, getCookie } from 'cookies-next'
+import { serialize } from "cookie"
+import dbConnect from "../../../lib/dbConnect"
+import Token from "../../../models/Token"
+
 
 export default async function handler(req, res) {
     const { method } = req
-
     await dbConnect()
 
     switch (method) {
-        case 'POST':
-            logout(req, res)
-            break
+        case 'GET':
+            return logout(req, res)
         default:
-            res.status(400).json({ message: "Method not allowed!" })
-            break
+            res.setHeader('Allow', ['GET'])
+            return res.status(405).end(`Method ${method} Not Allowed`)
     }
 }
 
 async function logout(req, res) {
+    const refreshToken = req.cookie.refresh_token
+    if (!refreshToken) {
+        console.log('no refresh token so you are already logged out')
+        return res.status(200).end('no refresh token so you are already logged out')
+    }
     try {
-
-        if (!hasCookie('authToken', { req, res })) {
-            return res.status(400).json({ message: 'Already logged out!' })
+        const tokenExists = await Token.findOne({ token: refreshToken })
+        if (!tokenExists) {
+            console.log('refresh token not found so you are already logged out')
+            return res.status(403).end('refresh token not found so you are already logged out')
         }
-        // Extract the jti claim from the auth token
-        const { jti } = jwt.decode(getCookie('authToken', { req, res }))
+        await tokenExists.remove()
+        const serializedRefresh = serialize("refresh_token", null, { httpOnly: true, sameSite: "strict", path: "/" })
+        const serializedAccess = serialize("access_token", null, { httpOnly: true, sameSite: "strict", path: "/" })
+        res.setHeader('Set-Cookie', [serializedAccess, serializedRefresh])
 
-        // Invalidate the jti in the blacklist
-        await fetch(`${process.env.HOST}/api/auth/blacklist`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ jti }),
-            credentials: 'include',
-        })
-
-        // Clear the auth token cookie
-        deleteCookie('authToken', { req, res })
-
-        res.status(200).json({ message: 'Logout successful' })
+        console.log("Logged out successfully")
+        return res.status(200).end('Logged out successfully')
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Internal server error' })
+        console.log(error)
+        return res.status(403).end(error.message)
     }
 }
