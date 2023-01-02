9e33eafcbd5e1f6672b4cb9345eb85a2f197a484
JethroSama <kel73896@gmail.com>
2023-01-02 21:06:58 +0800

    implement send message

---
+++ pages/api/chats/[chatId]/messages/index.js
@@ -0,0 +1,76 @@
+import dbConnect from "../../../../../lib/dbConnect"
+import verifyToken from "../../../../../lib/server/verifyToken"
+import Chat from "../../../../../models/Chat"
+import Message from "../../../../../models/Message"
+
+
+export default async function handler(req, res) {
+    const { method } = req
+
+    await dbConnect()
+
+    switch (method) {
+        case 'POST':
+            verifyToken(req, res, sendMessage)
+            break
+        case 'GET':
+            verifyToken(req, res, getMessages)
+            break
+        default:
+            res.setHeader('Allow', ['POST', 'GET'])
+            return res.status(405).end(`Method ${method} Not Allowed`)
+    }
+}
+
+async function sendMessage(req, res) {
+    try {
+        const userId = req.user.id
+        const chatId = req.query.chatId
+
+        const { text, attachments } = req.body
+
+        if (!chatId) {
+            return res.status(200).json({ error: { message: 'Chat id is required' } })
+        }
+
+        const chat = await Chat.findById(chatId)
+        if (!chat) {
+            return res.status(200).json({ error: { message: 'Chat not found' } })
+        }
+
+        const isUserInChat = chat.users.find(user => user.toString() === userId)
+
+        if (!isUserInChat) {
+            return res.status(200).json({ error: { message: 'User not in chat' } })
+        }
+
+        const message = new Message({
+            chat: chatId,
+            sender: userId,
+            text,
+            attachments,
+        })
+        await message.save()
+
+        chat.messages.push(message._id)
+        await chat.save()
+
+        return res.status(200).json({
+            error: null,
+            message: 'Message sent'
+        })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: { message: 'Internal server error' } })
+    }
+}
+
+async function getMessages(req, res) {
+    try {
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: { message: 'Internal server error' } })
+    }
+}
