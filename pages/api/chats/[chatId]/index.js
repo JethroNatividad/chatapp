8282192f22ac9dc4806d281dc082bb935149f083
JethroSama <kel73896@gmail.com>
2023-01-02 21:02:40 +0800

    rename chatId

--- pages/api/chats/[chatId]/index.js
+++ pages/api/chats/[chatId]/index.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 async function getChat(req, res) {
     try {
         const userId = req.user.id
-        const chatId = req.query.id
+        const chatId = req.query.chatId
 
         if (!chatId) {
             return res.status(200).json({ error: { message: 'Chat id is required' } })
