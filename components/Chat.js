ee5317878ef2aeea124d81cf034f90f66188dbc1
JethroNatividad <kel73896@gmail.com>
2023-02-02 20:07:40 +0800

    add hover effects

--- components/Chat.js
+++ components/Chat.js
@@ -3,7 +3,9 @@ import { Avatar, Text, Flex, Box } from '@chakra-ui/react'
 
 const Chat = () => {
     return (
-        <Flex px='3' py='2' align='center'>
+        <Flex px='3' py='2' align='center' cursor='pointer' _hover={ {
+            bg: 'blackAlpha.200'
+        } }>
             <Avatar name='User' />
             <Box flex='1' px='2'>
                 <Text fontSize='xl' >Username</Text>
