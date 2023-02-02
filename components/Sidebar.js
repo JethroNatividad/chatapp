67b4d27dcd042aee9dbfef7f9f143219da96c502
JethroNatividad <kel73896@gmail.com>
2023-02-02 19:48:37 +0800

    style sidebarheader

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,13 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
 import React from 'react'
 
 const Sidebar = () => {
     return (
-        <Box w='full'>Sidebar</Box>
+        <Box h='full' w='full' >
+            <Box px='2' py='3' shadow='sm' bg="blackAlpha.400">
+                <Button>Start a conversation</Button>
+            </Box>
+        </Box>
     )
 }
 
