1ad891def81a07f7b4880db427b88ad69ba9cff9
JethroNatividad <kel73896@gmail.com>
2023-02-02 22:22:07 +0800

    responsive sidebar header

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,8 +5,8 @@ import Chat from './Chat'
 const Sidebar = () => {
     return (
         <Flex flexDir='column' h='full' w='full' >
-            <Box px='2' py='3' shadow='sm' bg="blackAlpha.400">
-                <Button>Start a conversation</Button>
+            <Box px='2' py='3' h={ ['14', null, null, '20'] } shadow='sm' bg="blackAlpha.400">
+                <Button size={ ['sm', null, null, 'md'] }>Start a conversation</Button>
             </Box>
             <Box overflowY='scroll'>
                 <Chat />
