7d2911b263cb1ae712a1fc9c71f54f7e71f891ca
JethroSama <kel73896@gmail.com>
2023-02-04 17:46:56 +0800

    add props

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { Avatar, Text, Flex, Box } from '@chakra-ui/react'
 
-const Chat = ({ username, lastMessage }) => {
+const Chat = ({ username, last_message, last_message_at }) => {
     return (
         <Flex px='3' py='2' align='center' cursor='pointer' _hover={ {
             bg: 'blackAlpha.400'
         } }>
-            <Avatar name='User' />
+            <Avatar name={ username } />
             <Box flex='1' px='2'>
                 <Flex justifyContent='space-between'>
-                    <Text fontSize='xl' >Username</Text>
-                    <Text fontSize='sm' color='gray.500' ml='auto'>12:00</Text>
+                    <Text fontSize='xl' >{ username }</Text>
+                    <Text fontSize='sm' color='gray.500' ml='auto'>{ last_message_at }</Text>
                 </Flex>
-                <Text noOfLines={ 1 }>Last meddddddddddddddddddeashas ahsdjasdhahdshasdashdahsdhadh asdhad</Text>
+                <Text noOfLines={ 1 }>{ last_message }</Text>
             </Box>
         </Flex>
     )
