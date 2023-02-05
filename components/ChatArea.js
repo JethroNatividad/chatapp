6b1a3f4d8ee8c790bc341bcf1c5f821a428d7d4d
JethroNatividad <kel73896@gmail.com>
2023-02-05 22:38:08 +0800

    Chatarea use activechat

--- components/ChatArea.js
+++ components/ChatArea.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Box, Flex, Text } from '@chakra-ui/react'
+import { useChat } from '../context/ChatContext'
 
 
 const ChatArea = () => {
+    const { activeChat } = useChat()
+    if (!activeChat) {
+        return (
+            <Flex h='full' w='full' justifyContent='center' alignItems='center'>
+                <Text fontSize='lg'>Open or start a conversation</Text>
+            </Flex>
+        )
+    }
     return (
         <Flex h='full' direction="column" w='full'>
             <Box px='5' py='3' h={ ['14', null, null, '16'] } shadow='md'>
