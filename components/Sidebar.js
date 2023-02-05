e90f299d4e4990008da1aef0371a423f05139c5a
JethroNatividad <kel73896@gmail.com>
2023-02-05 22:54:22 +0800

    Add menu

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,4 +1,4 @@
-import { Box, Button, Flex } from '@chakra-ui/react'
+import { Box, Button, Flex, Menu, MenuItem, IconButton, MenuList, MenuButton } from '@chakra-ui/react'
 import React from 'react'
 import { useChat } from '../context/ChatContext'
 import { useCreateChat } from '../context/CreateChatContext'
@@ -10,9 +10,36 @@ const Sidebar = () => {
 
     return (
         <Flex direction='column' h='full' w='full' bg="blackAlpha.300">
-            <Box px='2' py='3' h={ ['14', null, null, '16'] } shadow='md' >
+            <Flex px='2' py='3' h={ ['14', null, null, '16'] } shadow='md' justifyContent='space-between'>
                 <Button onClick={ onOpen } size={ ['sm', null, null, 'md'] }>Create a conversation</Button>
-            </Box>
+                <Menu>
+                    <MenuButton
+                        variant='ghost'
+                        as={ IconButton }
+                        aria-label='Options'
+                        icon={ <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={ 1.5 } stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        }
+
+                    />
+                    <MenuList>
+                        <MenuItem>
+                            New Tab
+                        </MenuItem>
+                        <MenuItem>
+                            New Window
+                        </MenuItem>
+                        <MenuItem>
+                            Open Closed Tab
+                        </MenuItem>
+                        <MenuItem>
+                            Open File...
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </Flex>
             <Box overflowY='auto' flex={ 1 }>
                 { chatList.map(chat => (
                     <Chat handleClick={ () => setActiveChatId(chat.id) } key={ chat.id } last_message={ chat.last_message } username={ chat.name ? chat.name : chat.users.map((user) => user.username).join(", ") } />
