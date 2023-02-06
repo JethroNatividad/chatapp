cb674a3b3d710e1f78c872a13b1258ec807ba679
JethroSama <kel73896@gmail.com>
2023-02-06 23:05:45 +0800

    Style chat input

--- components/ChatArea.js
+++ components/ChatArea.js
@@ -20,7 +20,7 @@ const ChatArea = () => {
             <Box flex={ 1 }>
                 Chats
             </Box>
-            <Flex>
+            <Flex px="5" py="3">
                 <Input type='text' placeholder='Write a message..' /><IconButton icon={ <Icon h={ 5 } w={ 5 } color='white'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
