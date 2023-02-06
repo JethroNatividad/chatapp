d3f5f7360fa44bc0c49b1f9aa9ca3ea7048a9726
JethroSama <kel73896@gmail.com>
2023-02-06 23:09:01 +0800

    Move input t ocomponent

--- components/ChatArea.js
+++ components/ChatArea.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Box, IconButton, Flex, Input, Text, Icon } from '@chakra-ui/react'
+import { Box, Flex, Text } from '@chakra-ui/react'
 import { useChat } from '../context/ChatContext'
+import ChatInput from './ChatInput'
 
 
 const ChatArea = () => {
@@ -20,15 +21,9 @@ const ChatArea = () => {
             <Box flex={ 1 }>
                 Chats
             </Box>
-            <Flex px="5" py="3">
-                <Input type='text' placeholder='Write a message..' /><IconButton icon={ <Icon h={ 5 } w={ 5 } color='white'>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-                    </svg>
-
-                </Icon>
-                } variant='outline' />
-            </Flex>
+            <Box px="5" py="3">
+                <ChatInput />
+            </Box>
         </Flex>
 
     )
